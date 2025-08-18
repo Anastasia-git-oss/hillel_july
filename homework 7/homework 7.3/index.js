ca0ca153c119ca3a5365d00fcba83bcd97d09827
Anastasia T <tarasenkoanastasia66@hotmail.com>
2025-08-18 18:42:43 +0300

    add homework 8 and changed homework 7.3

--- homework 7/homework 7.3/index.js
+++ homework 7/homework 7.3/index.js
@@ -1,14 +1,12 @@
 function askNumber() {
     for (let i = 0; i < 10; i++) {
-        let valueUser = parseInt(prompt(`Enter number bigger than 100:`));
+        let valueUser = prompt(`Enter number bigger than 100:`);
       
-        if (valueUser > 100 ) {
+        if (valueUser > 100 || isNaN(valueUser) || i === 9 ) {
             alert(`Last entered number: ${valueUser}`);
-            return;
-        }
-        if (isNaN(valueUser)) {
-            alert(`You canceled the entry`);
-            return;
+            break;
+        }else if (valueUser <= 100){
+            alert(`Enter the correct number`)
         }
     }
    
